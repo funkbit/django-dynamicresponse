b1f1c4d4830b9c986a472db239e3f4afd47eb4d9
Robin Heggelund Hansen <skinneyz89@gmail.com>
2012-02-02 14:18:10 +0100

    Added unit-tests for DynamicResponse object

--- examples/myblog/blog/tests/response.py
+++ examples/myblog/blog/tests/response.py
@@ -4,9 +4,40 @@ from mock import Mock, patch
 from dynamicresponse.response import *
 
 
-class ResponseTest (unittest.TestCase):
+class DynamicResponseTest (unittest.TestCase):
 
-    @patch('dynamicresponse.response.HttpResponse')
-    def test_serialize_or_render(self, HttpResponse):
-        dynRes = SerializeOrRender('customers/list.html', { 'customers': 'test' })
-        self.assertTrue (isinstance(dynRes, DynamicResponse))
+    def test_serialize_returns_JsonResponse_when_status_is_200(self):
+        dynRes = DynamicResponse()
+        serialize_result = dynRes.serialize()
+
+        self.assertTrue(isinstance(serialize_result, JsonResponse))
+        self.assertTrue(isinstance(serialize_result, HttpResponse))
+        self.assertTrue(serialize_result.status_code == 200)
+
+    def test_serialize_returns_HttpResponse_when_status_is_not_200(self):
+        dynRes = DynamicResponse(status=CR_DELETED)
+        serialize_result = dynRes.serialize()
+
+        self.assertFalse(isinstance(serialize_result, JsonResponse))
+        self.assertTrue(isinstance(serialize_result, HttpResponse))
+        self.assertTrue(serialize_result.status_code == 204)
+
+        # second example to confirm
+        dynRes = DynamicResponse(status=CR_CONFIRM)
+        serialize_result = dynRes.serialize()
+
+        self.assertFalse(isinstance(serialize_result, JsonResponse))
+        self.assertTrue(isinstance(serialize_result, HttpResponse))
+        self.assertTrue(serialize_result.status_code == 405)
+
+    def test_full_context_returns_context_merged_with_extra_context(self):
+        testContext = {"testNum": 5, "word": "bird", "beach": 10}
+        testExtraContext = {"extra": True, "blue?": False}
+        dynResNoExtra = DynamicResponse(testContext)
+        dynResWithExtra = DynamicResponse(testContext, extra=testExtraContext)
+
+        for key, value in dynResNoExtra.full_context().items():
+            self.assertTrue(testContext[key] == value)
+
+        for key, value in dynResWithExtra.full_context().items():
+            self.assertTrue(testContext.get(key) == value or testExtraContext.get(key) == value)
