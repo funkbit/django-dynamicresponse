e1a608ef1e67030803e1689732287eb17fe294e1
Robin Heggelund Hansen <skinneyz89@gmail.com>
2012-02-03 11:34:27 +0100

    Added TestCase stubs to Api and DynamicFormat tests

---
+++ examples/myblog/blog/tests/api.py
@@ -0,0 +1,9 @@
+import unittest
+
+from dynamicresponse.middleware.api import *
+
+
+class ApiTest (unittest.TestCase):
+
+    def setUp(self):
+        pass
---
+++ examples/myblog/blog/tests/dynamicformat.py
@@ -0,0 +1,9 @@
+import unittest
+
+from dynamicresponse.middleware.dynamicformat import *
+
+
+class DynamicFormatTest (unittest.TestCase):
+
+    def setUp(self):
+        pass
