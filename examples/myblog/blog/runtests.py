5f92ec330eeb06407877219ad46f7c01fa553a1c
Jon Lønne <jon@funkbit.no>
2012-08-28 18:08:03 +0200

    Minor cleanup.

--- examples/myblog/blog/runtests.py
+++ examples/myblog/blog/runtests.py
@@ -1,12 +1,14 @@
 # This file mainly exists to allow python setup.py test to work.
 import os, sys
+
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myblog.settings'
 test_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, test_dir)
 
-from django.test.utils import get_runner
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myblog.settings'
+
 from django.conf import settings
+from django.test.utils import get_runner
 
 def runtests():
     TestRunner = get_runner(settings)
