7c9eaaa157be1b203efd571d3755ce43bf65b23f
Jon Lønne <jon@funkbit.no>
2013-06-06 15:56:09 +0200

    Cleaned up setup.py and license.

--- setup.py
+++ setup.py
@@ -13,18 +13,19 @@ if sys.argv[-1] == "publish":
     publish()
     sys.exit()
 
-setup(name='django-dynamicresponse',
-      version=__version__,
-      description='Lightweight framework for easily providing REST APIs for web apps built with Django.',
-      long_description=open('README.md').read(),
-      author='Funkbit AS',
-      author_email='post@funkbit.no',
-      url='https://github.com/funkbit/django-dynamicresponse',
-      packages=['dynamicresponse', 'dynamicresponse.middleware'],
-      tests_require=['django>=1.1,<1.4'],
-      test_suite='examples.myblog.blog.runtests.runtests',
-      license='BSD',
-      classifiers = (
+setup(
+    name='django-dynamicresponse',
+    version=__version__,
+    description='Lightweight framework for easily providing REST APIs for web apps built with Django.',
+    long_description=open('README.md').read(),
+    author='Funkbit',
+    author_email='post@funkbit.no',
+    url='https://github.com/funkbit/django-dynamicresponse',
+    packages=['dynamicresponse', 'dynamicresponse.middleware'],
+    tests_require=['django>=1.1,<1.4'],
+    test_suite='examples.myblog.blog.runtests.runtests',
+    license='BSD',
+    classifiers=(
         "Development Status :: 4 - Beta",
         "Framework :: Django",
         "Intended Audience :: Developers",
@@ -32,5 +33,5 @@ setup(name='django-dynamicresponse',
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        )
-     )
+    )
+)
